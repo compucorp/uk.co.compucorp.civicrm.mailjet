255ea17a8c1de64e8f30faec270173f31c390ab5
Erawat Chamanont <erawat@gmail.com>
2014-01-06 15:05:02 +0000

    Added Mailjet API for processing bounce mail.

---
+++ api/v3/Mailjet/ProcessBounces.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Mailjet.ProcessBounces API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_mailjet_processbounces_spec(&$spec) {
+  //$spec['magicword']['api.required'] = 1;
+}
+
+/**
+ * Mailjet.ProcessBounces API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_mailjet_processbounces($params) {
+  $lock = new CRM_Core_Lock('civimail.job.MailjetProcessor');
+  if (!$lock->isAcquired()) {
+    return civicrm_api3_create_error('Could not acquire lock, another MailjetProcessor process is running');
+  }
+  if (!CRM_Utils_Mail_EmailProcessor::processBounces()) {
+    $lock->release();
+    return civicrm_api3_create_error('Process Bounces failed');
+  }
+  $lock->release();
+
+  // FIXME: processBounces doesn't return true/false on success/failure
+  $values = array();
+  return civicrm_api3_create_success($values, $params, 'mailing', 'bounces');
+}
+
